e5fadebcae9d2b80b3646ac29bac45636d703e5f
beka2006 <gorgiladze.beka@kiu.edu.ge>
2026-01-30 23:33:40 +0400

    Use DATABASE_URL for Neon

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.postgresql://neondb_owner:npg_6fUL2GErzHMW@ep-dark-sky-ahog3ssp-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require,
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === "production"
     ? { rejectUnauthorized: false }
     : false,
 });
 
 module.exports = pool;
-
-
-
